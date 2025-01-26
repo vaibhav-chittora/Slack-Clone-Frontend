974b313ec087ae1394118b6f7f8077851f76c4c7
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2025-01-26 21:15:45 +0530

    added apis and react query hooks

---
+++ src/hooks/apis/auth/useSignUp.js
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import { signUpRequest } from "@/api/auth";
+
+export const useSignUp = () => {
+  const {
+    isPending,
+    isSuccess,
+    error,
+    mutate: signUpMutation,
+  } = useMutation({
+    mutationFn: signUpRequest,
+    onSuccess: (data) => {
+      console.log("Successfully signed up", data);
+    },
+    onError: (error) => {
+      console.log("Failed to sign up", error);
+    },
+  });
+
+  return { isPending, isSuccess, error, signUpMutation };
+};
