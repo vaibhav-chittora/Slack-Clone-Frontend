359f37d6aa7aeaf3807b76a6e77ab07ec071e65f
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2025-03-29 22:01:31 +0530

    implemented update workspace

--- src/components/molecules/CreateWorkspaceModal/CreateWorkspaceModal.jsx
+++ src/components/molecules/CreateWorkspaceModal/CreateWorkspaceModal.jsx
@@ -3,10 +3,14 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/u
 import { Input } from "@/components/ui/input"
 import { useCreateWorkspace } from "@/hooks/apis/workspaces/useCreateWorkspace"
 import { useCreateWorkspaceModal } from "@/hooks/context/useCreateWorkspaceModal"
+import { useQueryClient } from "@tanstack/react-query"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 export const CreateWorkspaceModal = () => {
+
+    const queryClient = useQueryClient()
+
     const { openCreateWorkspaceModal, setOpenCreateWorkspaceModal } = useCreateWorkspaceModal()
     const [workspaceName, setWorkspaceName] = useState("")
 
@@ -19,7 +23,8 @@ export const CreateWorkspaceModal = () => {
         try {
             const data = await createWorkspaceMutation({ name: workspaceName })
             console.log("Create the workspace with the data", data);
-            navigate(`/workspace/${data._id}`)
+            navigate(`/workspaces/${data._id}`);
+            queryClient.invalidateQueries("fetchWorkspaces")
         } catch (error) {
             console.log('Not able to create a workspace', error);
         } finally {
--- src/components/molecules/workspace/WorkspacePreferencesModal.jsx
+++ src/components/molecules/workspace/WorkspacePreferencesModal.jsx
@@ -1,33 +1,44 @@
 import { Button } from "@/components/ui/button"
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { useDeleteWorkspace } from "@/hooks/apis/workspaces/useDeleteWorkspace"
 import { useWorkspacePreferencesModal } from "@/hooks/context/useWorkspacePreferencesModal"
 import { useToast } from "@/hooks/use-toast"
 import { useQueryClient } from "@tanstack/react-query"
 import { TrashIcon } from "lucide-react"
+import { Input } from "@/components/ui/input"
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { useUpdateWorkspace } from "@/hooks/apis/workspaces/useUpdateWorkspace"
 
 export const WorkspacePreferencesModal = () => {
 
     const queryClient = useQueryClient()
     const [workspaceId, setWorkspaceId] = useState(null);
+
+    const [editOpen, setEditOpen] = useState(false);
+
     const navigate = useNavigate();
     const { toast } = useToast();
+
 
 
     const { initialValue, openPreferences, setOpenPreferences, workspace } = useWorkspacePreferencesModal();
     const { deleteWorkspaceMutation } = useDeleteWorkspace(workspaceId);
 
+    const { isPending, updateWorkspaceMutation } = useUpdateWorkspace(workspaceId);
+
+    const [renameValue, setRenameValue] = useState(workspace?.name);
+
     useEffect(() => {
         setWorkspaceId(workspace?._id)
+        setRenameValue(workspace?.name)
     }, [workspace])
 
     async function handleDeleteWorkspace() {
         try {
             await deleteWorkspaceMutation();
             navigate("/home");
-            queryClient.invalidateQueries("fetchWorkspaces");
+            queryClient.invalidateQueries(`fetchWorkspaceByID - ${workspace?._id}`);
             setOpenPreferences(false);
             toast({
                 title: "Workspace deleted successfully",
@@ -37,6 +48,25 @@ export const WorkspacePreferencesModal = () => {
             console.log("Error in deleting workspace", error)
             toast({
                 title: "Error in deleting workspace",
+                type: "error",
+            })
+        }
+    }
+
+    async function handleFormSubmit(e) {
+        e.preventDefault();
+        try {
+            await updateWorkspaceMutation(renameValue);
+            queryClient.invalidateQueries(`fetchWorkspaceByID - ${workspace?._id}`);
+            setOpenPreferences(false);
+            toast({
+                title: "Workspace updated successfully",
+                type: "success",
+            })
+        } catch (error) {
+            console.log("Error in updating workspace", error)
+            toast({
+                title: "Error in updating workspace",
                 type: "error",
             })
         }
@@ -52,26 +82,73 @@ export const WorkspacePreferencesModal = () => {
                     </DialogTitle>
                 </DialogHeader>
                 <div className="px-4 pb-4 flex flex-col gap-y-2">
-                    <div
-                        className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50 "
-                    >
-                        <div className="flex items-center justify-between">
-                            <p
-                                className="font-semibold text-sm"
-                            >
-                                Workspace Name
 
-                            </p>
-                            <p
-                                className="text-sm font-semibold hover:underline"
+                    <Dialog open={editOpen} onOpenChange={setEditOpen}>
+                        <DialogTrigger>
+
+                            <div
+                                className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50 "
                             >
-                                Edit
-                            </p>
-                        </div>
-                        <p className="text-sm font-semibold text-gray-500">
-                            {initialValue}
-                        </p>
-                    </div>
+                                <div className="flex items-center justify-between">
+                                    <p
+                                        className="font-semibold text-sm"
+                                    >
+                                        Workspace Name
+
+                                    </p>
+                                    <p
+                                        className="text-sm font-semibold hover:underline"
+                                    >
+                                        Edit
+                                    </p>
+                                </div>
+                                <p className="text-sm font-semibold text-gray-500">
+                                    {initialValue}
+                                </p>
+                            </div>
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle>
+                                    Rename Workspace
+                                </DialogTitle>
+                            </DialogHeader>
+                            <form className="space-y-4 " onSubmit={handleFormSubmit} >
+                                <Input
+                                    value={renameValue}
+                                    onChange={(e) => setRenameValue(e.target.value)}
+                                    disabled={isPending}
+                                    required
+                                    autoFocus
+                                    minLength={3}
+                                    maxLength={50}
+                                    placeholder="Workspace Name e.g. My Workspace"
+
+                                />
+                                <DialogFooter>
+                                    <DialogClose >
+
+                                        <Button
+                                            variant="outline"
+                                            disabled={isPending}
+                                        >
+                                            Cancel
+                                        </Button>
+                                    </DialogClose>
+
+                                    <Button
+                                        type="submit"
+                                        disabled={isPending}
+                                    >
+                                        Save
+                                    </Button>
+                                </DialogFooter>
+                            </form>
+                        </DialogContent>
+
+                    </Dialog>
+
+
                     <Button
                         variant="destructive"
                         className=" flex w-1/2 mt-4 items-center justify-center"
@@ -85,6 +162,6 @@ export const WorkspacePreferencesModal = () => {
 
                 </div>
             </DialogContent>
-        </Dialog>
+        </Dialog >
     )
 }
