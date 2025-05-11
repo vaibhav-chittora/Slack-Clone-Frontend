3c3d0bc09c4de22b48d1b52bfec3f11a557acb91
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2025-05-11 20:51:58 +0530

    added scroll for the message list

--- src/pages/Workspace/Channel/Channel.jsx
+++ src/pages/Workspace/Channel/Channel.jsx
@@ -7,7 +7,7 @@ import { useChannelMessages } from "@/hooks/context/useChannelMessages"
 import { useSocket } from "@/hooks/context/useSocket"
 import { useQueryClient } from "@tanstack/react-query"
 import { Loader2, TriangleAlertIcon } from "lucide-react"
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import { useParams } from "react-router-dom"
 
 export const Channel = () => {
@@ -23,6 +23,10 @@ export const Channel = () => {
 
     const { messageList, setMessageList } = useChannelMessages()
 
+    const messageContainerListRef = useRef(null)
+
+
+    // this useEffect is for joining the channel 
     useEffect(() => {
         if (!isFetching && !isError) {
             joinChannel(channelId)
@@ -30,18 +34,26 @@ export const Channel = () => {
 
     }, [isFetching, isError, joinChannel, channelId])
 
-
+    //this useEffect is to invalidate the query when channelId changes
     useEffect(() => {
         queryClient.invalidateQueries('getPaginatedMessages')
     }, [channelId])
 
+    //use-effect to set message list when channel messages are fetched
     useEffect(() => {
         if (isSuccess) {
             console.log("Channel Messages Fetched", messages)
             setMessageList(messages)
         }
 
-    }, [isSuccess, messages, setMessageList])
+    }, [isSuccess, messages, setMessageList, channelId])
+
+    // Scroll to the bottom of the message container when a new message is received
+    useEffect(() => {
+        if (messageContainerListRef.current) {
+            messageContainerListRef.current.scrollTop = messageContainerListRef.current.scrollHeight
+        }
+    }, [messageList])
 
     if (isFetching) {
         return (
@@ -63,16 +75,20 @@ export const Channel = () => {
         <div className="flex flex-col h-full">
             <ChannelHeader name={channelDetails?.name} />
 
-            {messageList?.map((message) => {
-                return <Message
-                    key={message._id}
-                    body={message?.body}
-                    authorImage={message?.senderId?.avatar}
-                    authorName={message?.senderId?.username}
-                    createdAt={message?.createdAt}
-                />
 
-            })}
+            <div ref={messageContainerListRef}
+                className="flex-5 overflow-y-auto p-4 gap-y-2">
+                {messageList?.map((message) => {
+                    return <Message
+                        className='scroll'
+                        key={message._id}
+                        body={message?.body}
+                        authorImage={message?.senderId?.avatar}
+                        authorName={message?.senderId?.username}
+                        createdAt={message?.createdAt}
+                    />
+                })}
+            </div>
 
             <div className="flex-1" />
             <ChatInput />
